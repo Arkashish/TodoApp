da319f08dbb96f51d7cd90ddcad326fac2b1d6c7
Arkashish Sutradhar <arkashishdhar1@gmail.com>
2024-09-13 20:05:40 +0530

    Removed the alert popup at starting

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-alert('page is workking')
+// alert('page is workking')
 const todoInput = document.getElementById("todoInput");
 const addTodoBtn = document.getElementById("addTodoBtn");
 const todoList = document.getElementById("todoList");
